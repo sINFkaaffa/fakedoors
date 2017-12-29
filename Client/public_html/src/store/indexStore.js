063da47b1e938179511a2ce5f774b14f98d29d18
sINFkaaffa <affa.karin@web.de>
2017-12-29 11:07:52 +0100

    add globale Variablen vuex

---
+++ Client/public_html/src/store/indexStore.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: "jippiei",
+    puh: "hurra"
+  },
+  mutations: {
+  	increment: state => state.count++,
+    decrement: state => state.count--
+  }
+})
