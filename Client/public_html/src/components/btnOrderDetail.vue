1fb44c6dbe27d7cec3109d7cdd7830182eac24eb
sINFkaaffa <32509544+sINFkaaffa@users.noreply.github.com>
2018-01-18 16:41:20 +0100

    Update btnOrderDetail.vue

--- Client/public_html/src/components/btnOrderDetail.vue
+++ Client/public_html/src/components/btnOrderDetail.vue
@@ -1,18 +1,12 @@
 <template>
-  <button v-on:click="orderDetail"><i class="material-icons center" >subdirectory_arrow_left</i>{{det}}</button>
+  <button v-on:click="orderDetail"><i class="material-icons center" >subdirectory_arrow_left</i></button>
 </template>
 
 <script>
 export default {
   name: 'btnOrderDetail',
   props:['orderId'],
-  data:{
-    det: false
-  },
   methods:{
-    detl: function(){
-      this.det=true
-    },
     orderDetail: function(){
       this.$emit('orderDetail', this.orderId)
     }
